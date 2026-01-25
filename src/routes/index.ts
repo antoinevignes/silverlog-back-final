af7240f779279e4825740006c00422ef94de820f
Antoine Vignes <168306302+antoinevignes@users.noreply.github.com>
2026-01-25 21:37:15 +0100

    route movieData OK

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import userRoute from "./user.route.js";
 import tmdbRoute from "./tmdb.route.js";
+import movieRoute from "./movie.route.js";
 
 const router = Router();
 
+router.use("/movies", movieRoute);
 router.use("/user", userRoute);
 router.use("/tmdb", tmdbRoute);
 
