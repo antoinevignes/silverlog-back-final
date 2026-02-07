ac830d9a82653cf108a4b32c70b0fa89b9b7a94b
Antoine Vignes <168306302+antoinevignes@users.noreply.github.com>
2026-02-07 19:13:54 +0100

    ajout des routes movie-state et rating

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import userRoute from "./user.route.js";
 import tmdbRoute from "./tmdb.route.js";
 import movieRoute from "./movie.route.js";
+import userMovieRoute from "./user-movie.route.js";
 
 const router = Router();
 
 router.use("/movies", movieRoute);
 router.use("/user", userRoute);
 router.use("/tmdb", tmdbRoute);
+router.use("/user_movie", userMovieRoute);
 
 export default router;
