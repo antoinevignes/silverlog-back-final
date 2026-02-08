98a61672d9e16201f82154656e2784bcee3317b0
Antoine Vignes <168306302+antoinevignes@users.noreply.github.com>
2026-02-08 22:33:41 +0100

    rajout route pour ping et maintenir le backend lancé

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,7 @@ import userRoute from "./user.route.js";
 import tmdbRoute from "./tmdb.route.js";
 import movieRoute from "./movie.route.js";
 import userMovieRoute from "./user-movie.route.js";
+import sql from "../db.js";
 
 const router = Router();
 
@@ -10,5 +11,12 @@ router.use("/movies", movieRoute);
 router.use("/user", userRoute);
 router.use("/tmdb", tmdbRoute);
 router.use("/user_movie", userMovieRoute);
-
+router.get("/health", async (req, res) => {
+  try {
+    await sql`SELECT 1`;
+    res.status(200).send("OK");
+  } catch (err) {
+    res.status(500).send("DB Error");
+  }
+});
 export default router;
